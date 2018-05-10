a95fbf43bab45bb3afc8e25fe63ce92f7c928167
Elise Griset <elise.griset@gmail.com>
2018-05-10 07:00:24 +0200

    movies db wooorks

--- models/user-model.js
+++ models/user-model.js
@@ -8,9 +8,9 @@ const userSchema = new Schema (
       type: Schema.Types.ObjectId,
       ref: 'MyList',
     },
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
+    username: {type: String},
+    email: {type: String},
+    password: {type: String},
     interestedIn: {type: [String]},
   },
   {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 const express = require ('express');
 const router = express.Router ();
+const axios = require ('axios');
 
-/* GET home page */
-router.get ('/', (req, res, next) => {
-  res.render ('index');
+//  GET MOVIES
+
+const moviesDb = axios.create ({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: '3d60d443da666373611cdcdb9db60537',
+    include_adult: false,
+  },
 });
+
+// List of movies in home page sorted by vote_count.desc
+
+router.get ('/', function (req, res, next) {
+  moviesDb
+    .get ('/discover/movie/', {
+      params: {
+        sort_by: 'vote_count.desc',
+      },
+    })
+    .then (result => {
+      console.log ('ca marche!!!!!! 🚀');
+      res.json (result.data);
+      // console.log (result.data);
+    })
+    .catch (err => {
+      console.log ('WTF ERROR 🚧');
+      // console.log (err);
+      next (err);
+    });
+});
+
+// details movie page
+
+// router.get ('/:id', function (req, res, next) {
+//   moviesDb
+//     .get (`/movie/${req.params.id}`)
+//     .then (response => {
+//       res.json (response.data);
+//     })
+//     .catch (err => {
+//       next (err);
+//     });
+// });
 
 module.exports = router;
