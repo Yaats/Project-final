5f2ccaf96a51651239e6af72c4c4f2b3569d2522
Yaats <trang.billy@live.fr>
2018-05-14 13:02:57 +0200

    obispo

--- routes/favorite-event.js
+++ routes/favorite-event.js
@@ -3,15 +3,24 @@ const favEvrouter = express.Router ();
 const Favorite = require ('../models/favorite-model');
 
 favEvrouter.post ('/:category', (req, res, next) => {
-  console.log('ezlae');
   Favorite.create ({
     user: req.user._id,
     category: req.params.category,
     title: req.body.nom,
+    o
   }).then (fav => {
     console.log(req.body.nom);
     res.json (fav);
   });
+});
+
+
+favEvrouter.get ('/billise', function (req, res, next) {
+  Favorite.findById()
+    .sort({createdAt: -1})
+    .then(listFromDb => {
+      res.json(listFromDb);
+    })
 });
 
 module.exports = favEvrouter;
